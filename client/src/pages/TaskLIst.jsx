18a00b7ace065b4e05bdf9f753baa1b32fedb7d1
@Rodril10 <luisenrique.rodvi@gmail.com>
2025-02-17 23:03:58 -0500

    fixes

--- client/src/pages/TaskLIst.jsx
+++ client/src/pages/TaskLIst.jsx
@@ -1,21 +1,32 @@
 import { useEffect, useState } from "react";
-import { getAllTask } from "../api/task.api"
-import { TaskCard } from "../components/taskCard";
-
+import { getAllTask } from "../api/task.api";
+import { useNavigate } from "react-router-dom";
 
 export function TaskList() {
   const [tasks, setTasks] = useState([]);
-  useEffect(()=>{
-    async function loadTask(){
+  const navigate = useNavigate(); // Mueve la declaración aquí
+
+  useEffect(() => {
+    async function loadTask() {
       const res = await getAllTask();
       setTasks(res.data);
     }
-    loadTask()
-  },[]);
+    loadTask();
+  }, []);
+
   return (
     <div className="grid grid-cols-3 gap-3">
       {tasks.map((task) => (
-        <TaskCard key={task.id} Task={task} />
+        <div
+          key={task.id} // Añadir un key único para cada elemento
+          className="bg-zinc-800 p-3 hover:bg-zinc-600 hover:cursor-pointer"
+          style={{ background: "gray" }}
+          onClick={() => navigate(`/task/${task.id}`)} // Cambia Task a task
+        >
+          <h1 className="font-bold uppercase">{task.title}</h1> {/* Cambia Task a task */}
+          <p>{task.description}</p> {/* Cambia Task a task */}
+          <hr />
+        </div>
       ))}
     </div>
   );
